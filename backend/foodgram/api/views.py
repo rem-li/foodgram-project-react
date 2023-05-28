48c9f867dd5058f6b408925fb2db2221a39b3833
rem-li <eliza.remli@gmail.com>
2023-05-28 19:57:12 +0300

    subs responce2

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -6,7 +6,7 @@ from api.serializers import (IngredientSerializer, RecipeCreateSerializer,
                              UserCreateSerializer, UserRecieveTokenSerializer,
                              UserSerializer)
 from django.core.cache import cache
-from django.db.models import Exists, F, OuterRef, Sum, Count
+from django.db.models import Count, Exists, F, OuterRef, Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
