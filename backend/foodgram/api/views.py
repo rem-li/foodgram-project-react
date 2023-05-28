393883d293628bc41156ab1e9516eadc566e4f94
rem-li <eliza.remli@gmail.com>
2023-05-28 15:35:59 +0300

    permissions

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -26,13 +26,11 @@ from users.models import User
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = [AllowAny]
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = [AllowAny]
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
