8421a2a9914198f3ef9bd05b29bfdd033a7848e9
rem-li <eliza.remli@gmail.com>
2023-05-29 21:17:21 +0300

    ingredient name search2

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -40,7 +40,9 @@ class RecipeFilter(django_filters.FilterSet):
 
 
 class IngredientFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    name = django_filters.CharFilter(
+        field_name='name', lookup_expr='icontains'
+    )
 
     class Meta:
         model = Ingredient
