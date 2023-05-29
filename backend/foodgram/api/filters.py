2a73277645722d69abd542f1c2dfed86473d3eec
rem-li <eliza.remli@gmail.com>
2023-05-29 20:30:25 +0300

    recipe filtration

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -8,7 +8,32 @@ class RecipeFilter(django_filters.FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
+    author = django_filters.CharFilter(
+        field_name='author__id'
+    )
+    is_favorited = django_filters.BooleanFilter(
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = django_filters.BooleanFilter(
+        method='get_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ['tags']
+        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if not user.is_authenticated:
+            return queryset.none()
+        if value:
+            return queryset.filter(favorite_recipes__user=user)
+        return queryset.exclude(favorite_recipes__user=user)
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if not user.is_authenticated:
+            return queryset.none()
+        if value:
+            return queryset.filter(shopping_lists__user=user)
+        return queryset.exclude(shopping_lists__user=user)
